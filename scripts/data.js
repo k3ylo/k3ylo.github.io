6f762b9e4a7970e5332c19c6df2781ac864afc88
k3ylo <161834260+k3ylo@users.noreply.github.com>
2026-02-19 21:05:27 +0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,36 +1,36 @@
 const configsData = [
   {
     id: 1,
-    name: "CSS x64",
-    fullName: "Counter-Strike: Source x64",
+    name: "css x64",
+    fullName: "steam version latest",
     lastUpdated: "2024-02-15",
     file_path: "./assets/configs/cssx64.zip"
   },
   {
     id: 2,
-    name: "CSS v34",
-    fullName: "Counter-Strike: Source v34",
+    name: "css v34",
+    fullName: "build 4044",
     lastUpdated: "2024-02-10",
     file_path: "./assets/configs/cssv34.zip"
   },
   {
     id: 3,
-    name: "CS:GO",
-    fullName: "Counter-Strike: Global Offensive",
+    name: "csgo",
+    fullName: "legasy version",
     lastUpdated: "2024-02-12",
     file_path: "./assets/configs/csgo.zip"
   },
   {
     id: 4,
-    name: "Classic Counter",
-    fullName: "Classic Counter-Strike",
+    name: "classic counter",
+    fullName: "version may 24, 2016",
     lastUpdated: "2024-02-08",
     file_path: "./assets/configs/cc.zip"
   },
   {
     id: 5,
-    name: "ClientMod",
-    fullName: "Counter-Strike ClientMod",
+    name: "clientmod",
+    fullName: "css v34, cm v3.0",
     lastUpdated: "2024-02-14",
     file_path: "./assets/configs/cm.zip"
   }
@@ -51,3 +51,4 @@ const videosData = [
   }
 
 ];
+
