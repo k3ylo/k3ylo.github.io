6ec21a23ad63f33ea22f0a6d5d42d4c13b35d5c6
k3ylo <161834260+k3ylo@users.noreply.github.com>
2026-03-20 20:54:35 +0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -37,83 +37,119 @@ const configsData = [
   }
 ];
 
-//videos
 // Данные для видео
 const videosData = [
-  // ========== ОСНОВНЫЕ РАБОТЫ ==========
+  // ========== ОСНОВНЫЕ РАБОТЫ (от новых к старым) ==========
   {
     id: 1,
     title: "chasm",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/gclXKMziQqk",
+    description: " `2560x1440
+24fps
+50000 bitrate`",
+    youtubeUrl: "https://mega.nz/file/IZdnSbCZ#fKgxQW0rDU4ixtiVN_DChkpPf6lBgLrJy-pcfSmOm28",
     date: "12.02.2026",
     downloadUrl: "https://mega.nz/твоя_ссылка_chasm" // замени на реальную
   },
   {
     id: 2,
     title: "dy570p14n",
-    description: "",
+    description: " `2560x1440
+24fps
+50000 bitrate`",
     youtubeUrl: "https://www.youtube.com/embed/Q1FT7iEmMVU",
     date: "30.12.2025",
-    downloadUrl: "" // добавь ссылку если есть
+    downloadUrl: "https://mega.nz/file/MV0xWLSJ#zTFbVN2GuAYI4Yxj704ypWOBwpsIUvLmVjWNKSo6hJk"
   },
   {
     id: 3,
     title: "flashbanged spray GIVES POWER!",
-    description: "",
+    description: " `2560x1440
+24fps
+50000 bitrate`",
     youtubeUrl: "https://www.youtube.com/embed/jJLCbiJGaTo",
     date: "01.10.2025",
-    downloadUrl: ""
+    downloadUrl: "https://mega.nz/file/ZR0BkJbD#EC_v3_0GTjYsKaCE6_gEzmsZte-ELTWDN1bx70b7Lug"
   },
   {
     id: 4,
-    title: "c3l35714l",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/jt02jQSQJjU",
-    date: "14.02.2025",
-    downloadUrl: ""
+    title: "wL main theme",
+    description: " `1920x1080
+60fps
+50000 bitrate`",
+    youtubeUrl: "https://www.youtube.com/embed/aTf06Z_bTvY",
+    date: "04.06.2025",
+    downloadUrl: "https://mega.nz/file/McVXDbDA#-sjbczzMG2Z-80HL0uo8Q_2WX4ZK5q_QeGMAKQ5fpME"
   },
   {
     id: 5,
-    title: "YEEPEEKAYE",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/DQAZIAnVaTs",
-    date: "03.12.2024",
-    downloadUrl: ""
+    title: "ЖИЗНЬ [community collab]",
+    description: " `1920x1080
+24fps
+50000 bitrate`",
+    youtubeUrl: "https://www.youtube.com/embed/FJQmJerA_ok",
+    date: "26.04.2025",
+    downloadUrl: "https://mega.nz/file/VAkxjZqY#Zn2Y_gqkOCVwQRZZKTJzoqo26Sqx0tQZimarbBarSPQ",
+    isCollaboration: true
   },
   {
     id: 6,
-    title: "без названия",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/9vBKSil6MCc",
-    date: "2025",
-    downloadUrl: ""
+    title: "c3l35714l",
+    description: " `2560x1440
+24fps
+50000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/jt02jQSQJjU",
+    date: "14.02.2025",
+    downloadUrl: "https://mega.nz/file/cQ82STCZ#Sg503wmMzNlX-pgn1tpq5PSDzPUCEVphkGID5kSnidg"
   },
   {
     id: 7,
-    title: "dhX0-WvYkk0",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/dhX0-WvYkk0",
-    date: "2025",
-    downloadUrl: ""
+    title: "saykiry by cinder",
+    description: " `2560x1440
+30fps
+35000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/pjOGWZY6w_E",
+    date: "03.02.2025",
+    downloadUrl: "https://mega.nz/file/5QcDXYzS#vQy50V1b8v7gtsIXOTqk3ZbQAdLZ1rkEv3uu14biiuk",
+    isCollaboration: true
   },
-  // ========== КОЛЛАБОРАЦИИ ==========
   {
     id: 8,
-    title: "ЖИЗНЬ [community collab]",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/FJQmJerA_ok",
-    date: "26.04.2025",
-    downloadUrl: "",
-    isCollaboration: true
+    title: "yo!",
+    description: " `2560x1440
+60fps
+40000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/9vBKSil6MCc",
+    date: "27.11.2024",
+    downloadUrl: "https://mega.nz/file/5UcCTRAb#A-NJcdwSkN8pHTGm9ugpplqWj5fbfCG6xfhKFqV9e-M"
   },
   {
     id: 9,
-    title: "saykiry by cinder",
-    description: "",
-    youtubeUrl: "https://www.youtube.com/embed/pjOGWZY6w_E",
-    date: "03.02.2025",
-    downloadUrl: "",
-    isCollaboration: true
+    title: "Lag Switch frags by Raelc - CS:S v34 CLIENTMOD",
+    description: " `1280x720
+60fps
+10000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/wJLN4QOHA8U",
+    date: "13.12.2024",
+    downloadUrl: "https://mega.nz/file/gd9XHDpK#ORd67NRASecTUD9P59cLYuDoXuzc5u0sUDqHg8RPkoM"
+  },
+  {
+    id: 10,
+    title: "YEEPEEKAYE",
+    description: " `2560x1440
+60fps
+50000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/DQAZIAnVaTs",
+    date: "03.12.2024",
+    downloadUrl: "https://mega.nz/file/9EUQDSpD#cZXLwV_1Hu8ujAJRLSCiwtko9J-w6ZXSUSyZvvtO6Go"
+  },
+  {
+    id: 11,
+    title: "raincoat",
+    description: " `2560x1440
+60fps
+70000 bitrate`",",
+    youtubeUrl: "https://www.youtube.com/embed/dhX0-WvYkk0",
+    date: "09.07.2024",
+    downloadUrl: "https://mega.nz/file/QE8j0a6S#zpFpt32U2yCFo_6UAbfMoA5XeM5LWxj8hDjtZPAfL5A"
   }
 ];
