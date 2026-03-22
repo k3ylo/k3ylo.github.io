e05b1ec16aa5865248f6c569a515f7234f5c3a03
k3ylo <161834260+k3ylo@users.noreply.github.com>
2026-03-22 05:57:08 +0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -4,35 +4,35 @@ const configsData = [
     id: 1,
     name: "css x64",
     fullName: "steam version (latest)",
-    lastUpdated: "22.03.2026",
+    lastUpdated: "2026-03-22",
     file_path: "./assets/configs/cssx64.zip"
   },
   {
     id: 2,
     name: "css v34",
     fullName: "build 4044",
-    lastUpdated: "22.03.2026",
+    lastUpdated: "2026-03-22",
     file_path: "./assets/configs/cssv34.zip"
   },
   {
     id: 3,
     name: "csgo",
     fullName: "legacy version",
-    lastUpdated: "19.02.2026",
+    lastUpdated: "2026-02-19",
     file_path: "./assets/configs/csgo.zip"
   },
   {
     id: 4,
     name: "classic counter",
     fullName: "version may 24, 2016",
-    lastUpdated: "19.02.2026",
+    lastUpdated: "2026-02-19",
     file_path: "./assets/configs/cc.zip"
   },
   {
     id: 5,
     name: "clientmod",
     fullName: "css v34, cm v3.0",
-    lastUpdated: "22.03.2026",
+    lastUpdated: "2026-03-22",
     file_path: "./assets/configs/cm.zip"
   }
 ];
