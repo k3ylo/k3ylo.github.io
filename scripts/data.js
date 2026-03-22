06bbc037dcd32d77cef26fdf5a803270f685722f
k3ylo <161834260+k3ylo@users.noreply.github.com>
2026-03-22 05:53:38 +0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -4,14 +4,14 @@ const configsData = [
     id: 1,
     name: "css x64",
     fullName: "steam version (latest)",
-    lastUpdated: "19.02.2026",
+    lastUpdated: "22.03.2026",
     file_path: "./assets/configs/cssx64.zip"
   },
   {
     id: 2,
     name: "css v34",
     fullName: "build 4044",
-    lastUpdated: "19.02.2026",
+    lastUpdated: "22.03.2026",
     file_path: "./assets/configs/cssv34.zip"
   },
   {
@@ -32,7 +32,7 @@ const configsData = [
     id: 5,
     name: "clientmod",
     fullName: "css v34, cm v3.0",
-    lastUpdated: "19.02.2026",
+    lastUpdated: "22.03.2026",
     file_path: "./assets/configs/cm.zip"
   }
 ];
